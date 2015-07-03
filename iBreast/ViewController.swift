5c2a8c7fdfc554e3affca8089c405f8028610730
martian <441699771@qq.com>
2015-07-03 09:28:12 +0800

    Initial Commit

---
+++ iBreast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBreast
+//
+//  Created by 钟其鸿 on 15/7/3.
+//  Copyright (c) 2015年 钟其鸿. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
